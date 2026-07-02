8318c3fbb9017a4bf8d1efe7e25b9bb9e2abb97b
DieterGJM <dieter@ultrabrokers.co.za>
2026-07-02 11:50:34 +0200

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl     = import.meta.env.VITE_SUPABASE_URL     || 'https://kvphvvjvmkkxkolaxeux.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2cGh2dmp2bWtreGtvbGF4ZXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDQ0NzAsImV4cCI6MjA5Nzc4MDQ3MH0.QSEGqQHeoVBM0s94cWAvoMXUsrMru5nIN-zcFfhXa4Q'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
